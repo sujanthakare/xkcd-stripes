b020085f1b71133035d30847b4276dbbb171a1f7
sujanthakare <sujandt@gmail.com>
2024-06-10 08:51:52 +0200

    feat: lazy loading

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,10 +1,37 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Root } from '@/pages/root';
-import { Details } from '@/pages/details';
+
+import { lazy, Suspense } from 'react';
+import { Skeleton } from './react-ui/skeleton';
+
+const Root = lazy(() => import('@/pages/root'));
+const Details = lazy(() => import('@/pages/details'));
+const Bookmarks = lazy(() => import('@/pages/bookmarks'));
 
 const router = createBrowserRouter([
-  { path: '/', element: <Root /> },
-  { path: '/details/:num', element: <Details /> },
+  {
+    path: '/',
+    element: (
+      <Suspense fallback={<Skeleton />}>
+        <Root />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/details/:num',
+    element: (
+      <Suspense fallback={<Skeleton />}>
+        <Details />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/bookmarks',
+    element: (
+      <Suspense fallback={<Skeleton />}>
+        <Bookmarks />
+      </Suspense>
+    ),
+  },
 ]);
 
 export function AppRoutes() {
